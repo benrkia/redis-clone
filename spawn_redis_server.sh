7bf20e6b1b673521a1a3ffccfd16f6a681d0076f
benrkia ilyasse <benrkyailyass@gmail.com>
2022-04-21 18:58:36 +0000

    Bind TCP socket to a PORT
    - bind socket to a port
    - update spwan_redis_server.sh to remove created tmp file

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -3,4 +3,5 @@ set -e
 tmpFile=$(mktemp -d)
 javac -sourcepath src/main/java src/main/java/Main.java -d "$tmpFile"
 jar cf java_redis.jar -C "$tmpFile"/ .
+rm -r "$tmpFile"
 exec java -cp java_redis.jar Main "$@"
