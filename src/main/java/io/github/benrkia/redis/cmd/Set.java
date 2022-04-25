0b72c94fb01326a7b3bfee9b58bed73fbb4e8216
benrkia ilyasse <benrkyailyass@gmail.com>
2022-04-25 02:25:25 +0000

    Implement setting key with an expiry via 'PX'

--- src/main/java/io/github/benrkia/redis/cmd/Set.java
+++ src/main/java/io/github/benrkia/redis/cmd/Set.java
@@ -5,24 +5,52 @@ import io.github.benrkia.redis.exception.InvalidCmdArguments;
 
 public class Set extends Cmd {
   private final DataSource dataSource;
+  private int px = -1;
 
   public Set(String... args) throws InvalidCmdArguments {
     super(args);
+    initArgs();
     dataSource = DataSource.instance();
   }
 
   @Override
   public String execute() {
-    dataSource.set(args[0], args[1]);
-    return protocol.simpleString("OK");
+    try {
+      if (px != -1)
+        dataSource.set(args[0], args[1], px);
+      else
+        dataSource.set(args[0], args[1]);
+
+      return protocol.simpleString("OK");
+    } catch (Exception e) {
+      return protocol.nil();
+    }
   }
 
   @Override
   protected String[] ensureValidArgs(String... args)
       throws InvalidCmdArguments {
-    if (args == null || args.length != 2)
+    if (args == null || (args.length != 2 && args.length != 4))
       throw new InvalidCmdArguments("wrong number of arguments for 'set' command");
+
+    if (args.length == 4) {
+      if (!"px".equalsIgnoreCase(args[2]))
+        throw new InvalidCmdArguments("invalid argument '" + args[2] + "' for 'set' command");
+
+      try {
+        if (Integer.parseInt(args[3]) <= 0)
+          throw new InvalidCmdArguments("invalid expire time in 'set' command");
+      } catch (NumberFormatException e) {
+        throw new InvalidCmdArguments("value is not an integer or out of range");
+      }
+    }
+
     return args;
+  }
+
+  private void initArgs() {
+    if (args.length == 4)
+      px = Integer.parseInt(args[3]);
   }
 
 }
--- src/main/java/io/github/benrkia/redis/datasource/DataSource.java
+++ src/main/java/io/github/benrkia/redis/datasource/DataSource.java
@@ -2,13 +2,17 @@ package io.github.benrkia.redis.datasource;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public final class DataSource {
-  private final Map<String, String> store;
+  private final Map<String, DataHolder> store;
+  private Queue<DataHolder> queue;
 
   private DataSource() {
     store = new ConcurrentHashMap<>();
+    queue = new PriorityBlockingQueue<>();
   }
 
   private static class BillPughSingleton {
@@ -21,12 +25,86 @@ public final class DataSource {
 
   public void set(final String key, final String value) {
     Objects.requireNonNull(key);
-    store.put(key, value);
+    put(key, new DataHolder(key, value));
+  }
+
+  public void set(final String key, final String value, int expiryTime) {
+    long expiresAt = now() + expiryTime;
+    put(key, new DataHolder(key, value, expiresAt));
   }
 
   public String get(final String key) {
     Objects.requireNonNull(key);
-    return store.get(key);
+    return getIfStillValid(store.get(key));
+  }
+
+  private void put(final String key, final DataHolder dataHolder) {
+    long now = now();
+
+    while (!queue.isEmpty()) {
+      DataHolder d = queue.peek();
+      if (!d.willExpiredBy(now))
+        break;
+      queue.poll();
+      store.remove(d.key);
+    }
+
+    store.put(key, dataHolder);
+    if (dataHolder.expiresAt != -1)
+      queue.add(dataHolder);
+  }
+
+  private String getIfStillValid(final DataHolder dataHolder) {
+    if (dataHolder == null)
+      return null;
+    if (!dataHolder.willExpiredBy(now()))
+      return dataHolder.value;
+
+    store.remove(dataHolder.key);
+    return null;
+  }
+
+  private long now() {
+    return System.currentTimeMillis();
+  }
+
+  private class DataHolder implements Comparable<DataHolder> {
+    private final String key;
+    private final String value;
+    private final long expiresAt;
+
+    DataHolder(String key, String value, long expiresAt) {
+      this.key = key;
+      this.value = value;
+      this.expiresAt = expiresAt;
+    }
+
+    DataHolder(String key, String value) {
+      this(key, value, -1);
+    }
+
+    boolean willExpiredBy(final long millis) {
+      return expiresAt != -1 && expiresAt <= millis;
+    }
+
+    @Override
+    public int hashCode() {
+      return key.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof DataHolder) {
+        DataHolder other = (DataHolder) obj;
+        return key.equals(other.key);
+      }
+      return false;
+    }
+
+    @Override
+    public int compareTo(DataHolder o) {
+      return Long.compare(expiresAt, o.expiresAt);
+    }
   }
 
 }
